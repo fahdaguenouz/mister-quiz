2aeaab2ac8afbae0c2c1134359143ae973756983
lfarssi <2002060600366@ofppt-edu.ma>
2024-09-29 19:39:56 +0200

    fds

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,7 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('username')->unique();            $table->string('email')->unique();
+            $table->string('username')->unique();
             $table->string('email')->unique();
             $table->string('password');
             $table->integer('xp')->default(0);
